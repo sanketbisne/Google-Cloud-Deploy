160f89d9cd35dca99fc90e4b25fcb05932101d65
Sanket bisne <38061560+sanketbisne@users.noreply.github.com>
2021-11-09 15:39:53 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+        "fmt"
+        "time"
+)
+
+func main() {
+        for {
+                fmt.Println("Hello sanket, Have Great Day,and enjoy !")
+
+                time.Sleep(time.Second * 1)
+        }
+}
